4f3ccfe2b801c15ea7bf6d3faba11ca75bbd03f9
Twinkle Chawla <twinkle1chawla@gmail.com>
2017-02-16 13:49:51 +0530

    jenkins test

---
+++ jenkinTestFile.sh
@@ -0,0 +1,7 @@
+#OnJenkins -> Click New Item
+#Choose the ‘Freestyle project option’
+#specify the details of the job
+#click on the Git option and enter the URL of the local git repository
+#click on the Add button for the credentials to add a user name and password to the github repository
+#Build section and click on Add build step
+#Build Now
